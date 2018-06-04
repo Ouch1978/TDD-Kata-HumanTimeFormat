66884c26afd4acc19c6f91e46ef8814a79b5852f
Ouch.Liu <ouch.liu@xuenn.com>
2018-06-04 14:31:41 +0800

    First commit.

---
+++ HumanTimeFormat/HumanTimeFormat/HumanTimeFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanTimeFormat
+{
+    public class HumanTimeFormatter
+    {
+        public HumanTimeFormatter()
+        {
+
+        }
+
+        public string Format( int seconds )
+        {
+            if( seconds == 0 )
+            {
+                return "now";
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ HumanTimeFormat/HumanTimeFormatTests/HumanTimeFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HumanTimeFormat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanTimeFormat.Tests
+{
+    [TestClass()]
+    public class HumanTimeFormatterTests
+    {
+        [TestMethod()]
+        public void TestInput0()
+        {
+            HumanTimeFormatter formatter = new HumanTimeFormatter();
+
+            string expected = "now";
+
+            string actual = formatter.Format( 0 );
+
+            Assert.AreEqual( expected , actual );
+        }
+    }
+}
